2e2d44fbf73575079013572e98b55ca57ffb29b5
Hind Boussaa <P2677680@my365.dmu.ac.uk>
2023-03-06 15:09:10 +0000

    findmethods

--- Skeleton/AdminSystem/CustomerDataEntry.aspx.cs
+++ Skeleton/AdminSystem/CustomerDataEntry.aspx.cs
@@ -4,12 +4,25 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
-    protected void btnOK_Click(object sender, EventArgs e)
+    private object txtName;
+
+    public int getType { get; private set; }
+    protected void Pgae_load(object sender, EventArgs e)
     {
+    }
+        protected void btnOK_Click(object sender, EventArgs e)
+    {
+        // create  a new instance of clsCustomer
+        clsCustomer customer = new clsCustomer();
+        //capture the Name
+        customer.Name= getType;
+        //store  customer in the session object
+        Session["customer"]=customer;
         //navigate to the viewr page
-        Response.Redirect("CustomerViewer.aspx");
+        Response.Redirect("_1Viewer.aspx");
     }
 }
