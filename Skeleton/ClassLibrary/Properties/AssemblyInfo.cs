16a213f88ea8627df0edc475afbaf7245f9d5b4c
Hind Boussaa <P2677680@my365.dmu.ac.uk>
2023-03-06 12:36:53 +0000

    Testing

--- Skeleton/ClassLibrary/Properties/AssemblyInfo.cs
+++ Skeleton/ClassLibrary/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -45,6 +46,8 @@ namespace ClassLibrary
         public int Phone { get; set; }
         public string Address { get; set; }
         public int Numberofcustomerhandled { get; set; }
+        public DateTime Date { get; set; }
+        public bool CustomerActive { get; set; }
     }
 
     public class clsCustome
