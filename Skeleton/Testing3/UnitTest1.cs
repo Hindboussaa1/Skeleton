948cb71392922dd841dee5d2f5ad69d67331f8b2
p2647603 <p2647603@GWHXX683WIN004W>
2023-03-07 13:47:46 +0000

    added test

---
+++ Skeleton/Testing3/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing3
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsStaff staff = new clsStaff();
+            Assert.isNotNull(staff);
+        }
+    }
+}
