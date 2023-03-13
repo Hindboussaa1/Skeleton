cabc0d96f3347bc620e21def8d3531af42bf634c
Hind Boussaa <P2677680@my365.dmu.ac.uk>
2023-03-13 11:56:17 +0000

    testing

--- Skeleton/AdminSystem/CustomerViewer.aspx1.cs
+++ Skeleton/AdminSystem/CustomerViewer.aspx1.cs
@@ -15,7 +15,7 @@ public partial class CustomerViewer : System.Web.UI.Page
         //create an instance  of clsCustomer
         clsCustomer Customer1 = new clsCustomer();
         //get the data  from the seesion object
-        clsCustomer Customer1 = (clsCustomer)Session["Customer1"];
+        Customer1 = (clsCustomer)Session["Customer1"];
         // display the name for this entry
         Response.Write(Customer1.Name);
 
