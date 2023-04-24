c8ec108e6c3f23fa932d675e667d05390a961e13
Hind Boussaa <P2677680@my365.dmu.ac.uk>
2023-04-25 00:08:56 +0100

    test

--- Skeleton/AdminSystem/CustomerDataEntry.aspx.cs
+++ Skeleton/AdminSystem/CustomerDataEntry.aspx.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -8,20 +10,102 @@ using ClassLibrary;
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
-   private string txtName;
+    private string txtName;
+    private int CustomerID;//textbox3
+    private string Address;//1
+    private string Email;//5
 
+    private DateTime Date;// 4
+    private bool check = false;//chkActive
     //public int getType { get; private set; }
-   
+
     protected void btnOK_Click(object sender, EventArgs e)
     {
-        txtName = TextBox2.Text;
-        // create  a new instance of clsCustomer
-        clsCustomer customer1 = new clsCustomer();
-        //capture the Name
-        customer1.Name = txtName;
-        //store  customer in the session object
-        Session["Customer"] = customer1;
-        //navigate to the viewr page
-        Response.Redirect("CustomerViewer.aspx");
+        
+        if (ValidateInput())
+        {
+            txtName = TextBox2.Text;
+
+            CustomerID = int.Parse(TextBox3.Text);
+            Address = TextBox1.Text;
+            Email = TextBox5.Text;
+            check = chkActive.Checked;
+            Date = DateTime.Parse(TextBox4.Text);
+
+            // create  a new instance of clsCustomer
+            clsCustomer customer1 = new clsCustomer();
+            //capture the Name
+            customer1.Name = txtName;
+            customer1.CustomerID = CustomerID;
+            customer1.Address = Address;
+            customer1.Email = Email;
+            customer1.CustomerActive = check;
+            customer1.Date = Date;
+            //store  customer in the session object
+            Session["Customer"] = customer1;
+            //navigate to the viewr page
+            Response.Redirect("CustomerViewer.aspx");
+        }
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    private bool ValidateInput()
+    {
+        bool isValid = true;
+
+        // Validate the txtName field
+        if (string.IsNullOrEmpty(TextBox2.Text))
+        {
+            CustomValidator cv = new CustomValidator("Name is required");
+            cv.ValidationGroup = "InputValidation";
+            cv.ControlToValidate = "txtName";
+            Page.Validators.Add(cv);
+            isValid = false;
+        }
+
+        // Validate the CustomerID field
+        int customerID;
+        if (!int.TryParse(TextBox3.Text, out customerID))
+        {
+            CustomValidator cv = new CustomValidator("Customer ID must be a number");
+            cv.ValidationGroup = "InputValidation";
+            cv.ControlToValidate = "TextBox3";
+            Page.Validators.Add(cv);
+            isValid = false;
+        }
+
+        // Validate the Address field
+        if (string.IsNullOrEmpty(TextBox1.Text))
+        {
+            CustomValidator cv = new CustomValidator("Address is required");
+            cv.ValidationGroup = "InputValidation";
+            cv.ControlToValidate = "Address";
+            Page.Validators.Add(cv);
+            isValid = false;
+        }
+
+        // Validate the Email field
+        if (!string.IsNullOrEmpty(TextBox5.Text))
+        {
+            Regex emailRegex = new Regex(@"^[^@\s]+@[^@\s]+\.[^@\s]+$");
+            if (!emailRegex.IsMatch(TextBox5.Text))
+            {
+                CustomValidator cv = new CustomValidator("Email is not valid");
+                cv.ValidationGroup = "InputValidation";
+                cv.ControlToValidate = "Email";
+                
+                isValid = false;
+            }
+        }
+
+        return isValid;
     }
 }
---
+++ Skeleton/ClassLibrary/CustomerCollection.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class CustomerCollection
+    {
+        // private member variable to store the list of customers
+        private List<clsCustomer> mCustomerList = new List<clsCustomer>();
+
+        // public property to return the list of customers
+        public List<clsCustomer> CustomerList
+        {
+            get
+            {
+                return mCustomerList;
+            }
+        }
+
+        // public method to add a new customer to the list
+        public void AddCustomer(clsCustomer customer)
+        {
+            mCustomerList.Add(customer);
+        }
+
+        // public method to remove a customer from the list
+        public void RemoveCustomer(clsCustomer customer)
+        {
+            mCustomerList.Remove(customer);
+        }
+
+        // public method to find a customer by ID
+        public clsCustomer FindCustomerByID(int customerID)
+        {
+            foreach (clsCustomer customer in mCustomerList)
+            {
+                if (customer.CustomerID == customerID)
+                {
+                    return customer;
+                }
+            }
+            // if customer not found, return null
+            return null;
+        }
+
+        // public method to find customers by email address
+        public List<clsCustomer> FindCustomersByEmail(string email)
+        {
+            List<clsCustomer> customers = new List<clsCustomer>();
+            foreach (clsCustomer customer in mCustomerList)
+            {
+                if (customer.Email == email)
+                {
+                    customers.Add(customer);
+                }
+            }
+            return customers;
+        }
+
+        // public method to find active customers
+        public List<clsCustomer> FindActiveCustomers()
+        {
+            List<clsCustomer> customers = new List<clsCustomer>();
+            foreach (clsCustomer customer in mCustomerList)
+            {
+                if (customer.CustomerActive)
+                {
+                    customers.Add(customer);
+                }
+            }
+            return customers;
+        }
+
+        // public method to find inactive customers
+        public List<clsCustomer> FindInactiveCustomers()
+        {
+            List<clsCustomer> customers = new List<clsCustomer>();
+            foreach (clsCustomer customer in mCustomerList)
+            {
+                if (!customer.CustomerActive)
+                {
+                    customers.Add(customer);
+                }
+            }
+            return customers;
+        }
+    }
+}
