32cf36766467dcbd54b68ecfba3e1153a9b00b4e
P2646750 <P2646750@my365.dmu.ac.uk>
2023-04-27 14:22:43 +0100

    StockData

---
+++ Skeleton/ClassLibrary/clsSstock.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class clsSstock
+    {
+        public bool Active { get; set; }
+        public DateTime DateAdded { get; set; }
+        public int CarId { get; set; }
+        public string CarBrand { get; set; }
+        public string CarModel { get; set; }
+        public string CarColor { get; set; }
+    }
+}
---
+++ Skeleton/Testing2/tstStock.cs
@@ -0,0 +1,69 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing2
+{
+    [TestClass]
+    public class tstStock
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsSstock StokManage = new clsSstock();
+            Assert.IsNotNull(StokManage);
+        }
+        [TestMethod]
+        public void ActiveStock()
+        {
+            clsSstock StockManage = new clsSstock();
+            Boolean TestData = true;
+            StockManage.Active = TestData;
+            Assert.AreEqual(StockManage.Active, TestData);
+
+        }
+        [TestMethod]
+        public void CarYearkOK()
+        {
+            clsSstock StockManage = new clsSstock();
+            DateTime TestData = DateTime.Now.Date;
+            StockManage.DateAdded = TestData;
+            Assert.AreEqual(StockManage.DateAdded, TestData);
+        }
+        [TestMethod]
+        public void CarIdStockOK()
+        {
+            clsSstock StockManage = new clsSstock();
+            Int32 TestData = 1;
+            StockManage.CarId = TestData;
+            Assert.AreEqual(StockManage.CarId, TestData);
+        }
+        [TestMethod]
+        public void CarBrandOK()
+        {
+            clsSstock StockManage = new clsSstock;
+            string TestData = "Mercedes";
+            StockManage.CarBrand = TestData;
+            Assert.AreEqual(StockManage.CarBrand, TestData);
+        }
+        [TestMethod]
+        public void CarModelOK() 
+        {
+            clsSstock StockManage = new clsSstock();
+            string TestData = "E320";
+            StockManage.CarModel = TestData;
+
+            Assert.AreEqual(StockManage.CarModel, TestData);
+        }
+        [TestMethod]
+        public void CarColorOK()
+        {
+            clsSstock StockManage = new clsSstock();
+            string TestData = "Black";
+            StockManage.CarColor = TestData;
+            Assert.AreEqual(StockManage.CarColor, TestData);
+        }
+    }
+
+
+}
